33f3006da260982c8b69b76c4026671b667475d0
liwei <liw>
2020-10-18 12:02:15 +0800

    1

--- www/clock.js
+++ www/clock.js
@@ -3,9 +3,9 @@ var ctx = dom.getContext('2d');//创建context对象
 
 
  
- dom.width = window.innerWidth;
+//  dom.width = window.innerWidth - 50;
 //  dom.height = window.innerHeight;
- var width = ctx.canvas.width;//canvas宽度
+ var width = ctx.canvas.width ;//canvas宽度
  var height = ctx.canvas.height;//canvas高度
  console.log(width,height)
 
@@ -34,35 +34,35 @@ function draw(time){
 
 	ctx.clearRect(0,0,width,height)
 
-	var dJ = 0,dX =0;
-	for (var  j = 0; j < height; j += dJ) {
-		dJ = 8 + 5 * Math.abs(Math.cos(time/500  + dX / 15 + dJ/15 +  j/50));
-		for (var index = 0 ; index < width; index += dX  ) {
-			dX = 8 + 5 * Math.abs( Math.sin(time/500  + dJ / 15 + dX/15  + j/40 + index/50))
-			y = j;
+	// var dJ = 0,dX =0;
+	// for (var  j = 0; j < height; j += dJ) {
+	// 	dJ = 8 + 5 * Math.abs(Math.cos(time/500  + dX / 15 + dJ/15 +  j/50));
+	// 	for (var index = 0 ; index < width; index += dX  ) {
+	// 		dX = 8 + 5 * Math.abs( Math.sin(time/500  + dJ / 15 + dX/15  + j/40 + index/50))
+	// 		y = j;
 
-			// ctx.fillStyle = `rgb(${ Math.abs(  Math.sin(Math.cos(time / 510)  + ( 2 * index +  j +  time/7 ) / 150)) * 255},${Math.abs(Math.cos(Math.sign(time/500)  + ( index + 2 * j +  time / 8) / 150)  ) * 255},${Math.abs(Math.cos( Math.sign(time/500) +  ( index + j + time / 9) / 150) ) * 255})`;
-			ctx.fillStyle = '#c3c3c3';
-			ctx.fillRect(index,y   ,dX - 2 ,dJ -2);
-	   }
+			  
+	// 		ctx.fillStyle = '#c3c3c3';
+	// 		ctx.fillRect(index,y   ,dX - 2 ,dJ -2);
+	//    }
 
-	}
+	// }
 
 	
 	time /= 10;
 	let start =   0 ;
 	
 	let color = 0x0e0e0e;
-	for (let angel = start; angel <  56.28; angel += 0.01) {
+	for (let angel = start; angel <  39.8; angel += 0.01) {
 
-		let r = 15 * (3 +5 *  Math.sin(angel + time / 50) +  1 *  Math.cos(angel * angel + time/61) +  0.5 * Math.sin(time / 47) )
+		
+		var r =   angel * 2.5
  
-
-		var x =   r * Math.cos( angel );
-		var  y =   r * Math.sin( angel  );
+		var x =   r * Math.sin( angel  + time / 28);
+		var  y =   r * Math.cos( angel + time/27);
  
-		y += 50 +  (height - 90) * Math.abs(Math.cos( time/189 ))
-		x += 50 +   (width - 90) * Math.abs(Math.sin(time/197 ))
+		y +=  height/2; +   (height - 50) * Math.abs(Math.cos( time/189 )) 
+		x +=  width /2 ;+   (width - 50) * Math.abs(Math.sin(time/197 ))
 
 
 		if(x < 0){
@@ -104,11 +104,62 @@ function draw(time){
 		}
 		y = Math.floor(y);
 
- 
-		ctx.fillStyle =  '#000000'
+		ctx.fillStyle =  '#000'
 
 		color += 0x0e0e0e;
 		ctx.fillRect(x ,y   ,1,1);
+
+
+
+		r = 6 * ( 2 + 6 *  Math.sin(angel + time / 30) +  4 *  Math.cos(angel * angel + time/61) +  2 * Math.cos(angel + time / 37) + 2 * Math.sin(angel * angel + time / 37) );
+
+		x =   r * Math.cos( angel );
+		y =   r * Math.sin( angel );
+	 
+		y +=  height/2; +   (height - 50) * Math.abs(Math.cos( time/189 )) 
+		x +=  width /2 ;+   (width - 50) * Math.abs(Math.sin(time/197 ))
+	
+	
+		if(x < 0){
+			const q = Math.floor(-x /width) %2;
+			if(q== 1){
+				x = width - (-x%width);
+			}
+			else {
+				x = -x % width;
+			}
+		}
+		else if(x > width){
+			const q = Math.floor(x /width) %2;
+			if(q== 1){
+				x = width - (x%width);
+			}
+			else {
+				x = x % width;
+			}
+		}
+	
+		if (y > height) {
+			const q = Math.floor(y /height) %2;
+			if(q== 1){
+				y = height - (y%height);
+			}
+			else {
+				y = y % height;
+			}
+	
+		}else if(y < 0){
+			const q = Math.floor(-y /height) %2;
+			if(q== 1){
+				y = height - (-y%height);
+			}
+			else {
+				y = -y % height;
+			}
+		}
+		ctx.fillStyle =  '#000000'
+		ctx.fillRect(x ,y   ,1,1);
+		
    }
 	 
    requestAnimationFrame(draw)
