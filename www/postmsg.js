31c902f1c5ac913fe30d85058c34de4d56386bd3
liw003 <liw003>
2022-09-25 13:01:44 +0800

    00

--- www/postmsg.js
+++ www/postmsg.js
@@ -1,8 +1,23 @@
+!function(){
+
  
+    let html = `
+    <div  class="a904"></div>
+    <div id="a903"  class="a901">回到主页</div>
+    <div id="a901"  class="a901">提交留言</div>
+    <textarea class="a902" name="" value='' placeholder="留言" id="a902"  ></textarea>
+    <p>
+    <img style='width:200px'  src='//imxsw.herokuapp.com' ></img>
+    `
+    
+    document.write(html)
+
+
 var sc2 = document.createElement('script');
 sc2.src = location.protocol + '//libs.baidu.com/jquery/2.0.0/jquery.min.js'
 
 document.body.appendChild(sc2);
+
 
 sc2.onload = function(){
     if(location.href.indexOf('list')> 0 ){
@@ -12,8 +27,7 @@ sc2.onload = function(){
         $('body').prepend(div);
     }
     
-    var sc3 = document.createElement('script');
-    sc3.text = `
+    
     $('#a903').click(function(){
         location.href = "/"
     })
@@ -27,32 +41,18 @@ sc2.onload = function(){
         if(msg){
             var url = '//vee.cyclic.app/postcomment?msg=' +encodeURIComponent(msg);
             $.get(url,function(data){
-                alert(JSON.stringify(data,null,'\t'));·
+                alert(JSON.stringify(data,null,'\t'));
                 location.reload();
-            });·
+            });
         }else{
             alert('写点啥吧,哪怕一个字呢')
         }
 			 
-    })`
-
-    document.body.appendChild(sc3);
+    })
 }
 
- 
-let html = `
-<div  class="a904"></div>
-<div class='a901' id="hexopage">hexo 主页</div>
-
-<div id="a903"  class="a901">回到主页</div>
-<div id="a901"  class="a901">提交留言</div>
-<textarea class="a902" name="" value='' placeholder="留言" id="a902"  ></textarea>
-<p>
-<img style='width:200px'  src='//imxsw.herokuapp.com' ></img>
-`
-
-document.write(html)
  
 
 // window.document.body.insertAdjacentHTML('beforeend',html)
 
+}()
